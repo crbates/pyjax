d8b6f518cb319b914c86a757e20306840d0ca1fd
Cameron Bates <cameron.r.bates@gmail.com>
2012-05-10 10:15:18 -0700

    remove dependency on simplejson use built-in json method instead

--- ajax_app.py
+++ ajax_app.py
@@ -1,17 +1,17 @@
 """
-ajax_app.py
+This is a simple 
 
 Demo of AJAX using:
 	- cherrypy
 	- simplejson
 	- jquery
+	- flot
 """
 import cherrypy
-import webbrowser
 import os
-import simplejson
-import sys
+import json
 import numpy as np
+
 MEDIA_DIR = os.path.join(os.path.abspath("."), u"media")
 
 class AjaxApp(object):
@@ -26,7 +26,7 @@ class AjaxApp(object):
         cherrypy.response.headers['Content-Type'] = 'application/json'
         y = np.random.normal(50,25,300)
         self.y = self.y + np.histogram(y,300)[0]
-        return simplejson.dumps(dict(title=name,val = list(self.y)))
+        return json.dumps(dict(title=name,val = self.y.tolist()))
 
 config = {'/media':
                 {'tools.staticdir.on': True,
@@ -37,6 +37,7 @@ config = {'/media':
 #def open_page():
 #    webbrowser.open("http://127.0.0.1:8080/")
 #cherrypy.engine.subscribe('start', open_page)
-cherrypy.tree.mount(AjaxApp(), '/', config=config)
-cherrypy.engine.start()
+#cherrypy.tree.mount(AjaxApp(), '/', config=config)
+#cherrypy.engine.start()
 #cherrypy.quickstart(AjaxApp())
+cherrypy.quickstart(AjaxApp(),config=config)
